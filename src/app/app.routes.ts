cad54ca62bba13d6d128f94db69e375e3545795d
alejandro.espinola <alejandroespinolahernandez@gmail.com>
2026-05-07 12:55:10 +0200

    make routes

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,13 @@
 import { Routes } from '@angular/router';
+import { Cataloge } from './cataloge/cataloge';
+import { Add } from './add/add';
+import { Delete } from './delete/delete';
+import { Modify } from './modify/modify';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: 'cataloge', component:  Cataloge},
+  { path: 'add', component: Add },
+  { path: 'delete', component: Delete },
+  { path: 'modify', component: Modify },
+  { path: '', redirectTo: '/home', pathMatch: 'full' }
+];
